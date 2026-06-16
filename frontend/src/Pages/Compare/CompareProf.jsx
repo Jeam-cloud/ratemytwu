0c2d07c0302bed64025bbfaf488d747a05c741a6
Jeam-cloud <213903051+Jeam-cloud@users.noreply.github.com>
2026-06-16 13:50:58 -0700

    included review count and bug fix in compare page

--- frontend/src/Pages/Compare/CompareProf.jsx
+++ frontend/src/Pages/Compare/CompareProf.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { API_URL } from "../../config"
 
@@ -7,9 +7,41 @@ export default function CompareProf() {
     const [firstProf, setFirstProf] = useState(null)
     const [secondProf, setSecondProf] = useState(null)
 
-    const [firstInput, setFirstInput] = useState(null)
-    const [secondInput, setSecondInput] = useState(null)
+    const [firstInput, setFirstInput] = useState("")
+    const [secondInput, setSecondInput] = useState("")
 
+    const [error, setError] = useState("")
+
+    const handleFirstProf = async () => {
+        const response = await fetch(`${API_URL}/professor/?search_professor=${firstInput}`, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        })
+        const data = await response.json()
+
+        if (!data.length) {
+            setError("First professor not found")
+            return
+        }
+        setError("")
+        setFirstProf(data[0])
+    }
+
+    const handleSecondProf = async () => {
+        
+        const response = await fetch(`${API_URL}/professor/?search_professor=${secondInput}`, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        })
+        const data = await response.json()
+
+        if (!data.length) {
+            setError("Second professor not found")
+            return
+        }
+        setError("")
+        setSecondProf(data[0])
+    }
 
     return(
         <>
@@ -21,6 +53,8 @@ export default function CompareProf() {
                     value={firstInput}
                     onChange={(e) => setFirstInput(e.target.value)}
                 />
+                
+                <button onClick={handleFirstProf}>Search</button>
             </div>
 
             <div>
@@ -28,26 +62,34 @@ export default function CompareProf() {
                 <input
                     type="text"
                     placeholder="Second Professor"
-                    value={seconInput}
+                    value={secondInput}
                     onChange={(e) => setSecondInput(e.target.value)}
                 />
+                <button onClick={handleSecondProf}>Search</button>
             </div>
 
             {error && <p>{error}</p>}
-            
-            <div>
-                {firstProf.name}
-                <p>rating: {firstProf.average_rating}</p>
-                <p>difficulty: {firstProf.average_difficulty}</p>
-                <p>take again: {firstProf.average_take_again}</p>
-            </div>
 
-            <div>
-                {secondProf.name}
-                <p>rating: {secondProf.average_rating}</p>
-                <p>difficulty: {secondProf.average_difficulty}</p>
-                <p>take again: {secondProf.average_take_again}</p>
-            </div>
+            {firstProf && secondProf && (
+                <div>
+                    <div>
+                        {firstProf.name}
+                        <p>rating: {firstProf.average_rating}</p>
+                        <p>difficulty: {firstProf.average_difficulty}</p>
+                        <p>take again: {firstProf.average_take_again}</p>
+                        <p>reviews: {firstProf.review_count}</p>
+                    </div>
+
+                    <div>
+                        {secondProf.name}
+                        <p>rating: {secondProf.average_rating}</p>
+                        <p>difficulty: {secondProf.average_difficulty}</p>
+                        <p>take again: {secondProf.average_take_again}</p>
+                        <p>reviews: {secondProf.review_count}</p>
+                    </div>
+                </div>
+            )}
+
         </>
     )
 }
